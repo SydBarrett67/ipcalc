b2a7dd2f722511edce3253687e4d206769cce079
Alessandro Di Luca <email.recuperoxearth@gmail.com>
2026-02-17 18:19:31 +0100

    added InputHandler#

--- Ipcalc.java
+++ Ipcalc.java
@@ -6,38 +6,85 @@ public class Ipcalc {
     public static void main(String[] args) {
         String currVersion = "0.03";
 
-        if (args.length == 0) {
-            System.out.println("Use -help for command list.");
-            return;
+        try {
+            // Input validation
+            if (args.length == 0) throw new Exception("Try -help / -h for command list.");
+            if (args[0] == "-h" || args[0] == "-help") throw new Exception("");
+            InputHandler.validate(args[0]);
+
+            // Parsing
+            String[] parts = args[0].split("/");
+            IpAddress ip = new IpAddress(parts[0]);
+            
+            Netmask mask;
+            if (parts.length < 2) {
+                OutputHandler.showWarning("Netmask non specificata. Generazione in corso...");
+                mask = new Netmask(ip.getDefaultPrefix());
+            } else {
+                mask = new Netmask(Integer.parseInt(parts[1]));
+            }
+
+            // 3. Output
+            OutputHandler.printRow("Indirizzo IP", ip);
+            OutputHandler.printRow("Netmask", mask);
+
+        } catch (Exception e) {
+            OutputHandler.showError(e.getMessage());
+            System.exit(1);
         }
 
-        if (args.length > 2) {
-            System.out.println("Too many arguments.");
-            return;
-        }
-
-        else if (args[0].equals("-help") || args[0].equals("-h")) {
-            System.out.println("Command list:"
-                    + "\n-help || -h: command list"
-                    + "\n-version: show current version"
-                    + "\n-ipcalc <ip>: calculate IP");
-            return;
-        }
-
-        else if (args[0].equals("-version") || args[0].equals("-v")) {
+        if (args[0].equals("-version") || args[0].equals("-v")) {
             System.out.println("v" + currVersion);
             return;
         }
+    }
 
-        else {
-            Address ip = new Address(args[1]);
-            System.out.println("Decimal dotted IP: "+ip.getDecimalDottedQuads());
-            System.out.println("Integer IP (long): "+ip.getIntIP());
-            System.out.println("Binary dotted IP: "+ip.getBinaryDotted());
+    // Input Handler class
+    public abstract class InputHandler {
+        public static void isValidIP(String in) {
+            if (in = null || in.isEmpty()) {
+                throw new Exception("Too few arguments. Try -help / -h for command list.");
+            }
+            if (!input.matches("[0-9./]+")) {
+                throw new Exception("Illegal characters.");
+            }
+
+            String[] parts = in.split("/");
+
+            validateIpFormat(parts[0]);
+
+            if (parts.length > 1) {
+                validateCidrFormat(parts[1]);
+            }
+
+            if (parts.length > 2) {
+                throw new Exception("Illegal IP format.");
+            }
+        }
+
+        private static void validateIpFormat(String ip) throws Exception {
+            String[] octets = ip.split("\\.");
+            if (octets.length != 4) {
+                throw new Exception("Illegal format.");
+            }
+        }
+
+        private static void validateCidrFormat(String cidr) throws Exception {
+            try {
+                int p = Integer.parseInt(cidr);
+                if (p < 0 || p > 32) throw new Exception();
+            } catch (Exception e) {
+                throw new Exception("Illegal CIDR format.");
+            }
         }
     }
 
-    
+    public abstract class OutputHandler {
+
+    }
+
+
+    // Address class: conversions etc.
     public static class Address {
 
         private String strIP;
